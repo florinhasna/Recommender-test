07b04ec857cd2773f072ecde2a15220798fd3f88
florinhasna <123017602+florinhasna@users.noreply.github.com>
2025-02-17 17:10:01 +0000

    Update arch.py

--- arch.py
+++ arch.py
@@ -9,4 +9,4 @@ arch_c = []
 connector_function = "full_conn"
 
 # To maintain compatibility with our API, do not change the variable name "Arch" or the constructor class "ao.Arch" in the line below (the API is pre-loaded with a version of the Arch class in this repo's main branch, hence "ao.Arch")
-arch = ar.Arch(arch_i, arch_z, arch_c, connector_function, description)
+Arch = ar.Arch(arch_i, arch_z, arch_c, connector_function, description)
