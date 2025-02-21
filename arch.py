652a4ea1a25e4edc30d1de6ffcb8ac29de704672
florinhasna <123017602+florinhasna@users.noreply.github.com>
2025-02-21 22:35:54 +0000

    Update arch.py

--- arch.py
+++ arch.py
@@ -3,10 +3,11 @@ import ao_arch as ar
 description = "Basic Recommender System"
 
 #genre, length
-arch_i = [5, 2, 3, 1, 2]   # genre_encoding, age_category, mood, ana (animation or not), satisfaction_level
-arch_z = [1]           
+arch_i = [100]   # genre_encoding, age_category, mood, ana (animation or not), satisfaction_level
+arch_z = [10]           
 arch_c = []           
 connector_function = "full_conn"
 
 # To maintain compatibility with our API, do not change the variable name "Arch" or the constructor class "ao.Arch" in the line below (the API is pre-loaded with a version of the Arch class in this repo's main branch, hence "ao.Arch")
+
 Arch = ar.Arch(arch_i, arch_z, arch_c, connector_function, description)
